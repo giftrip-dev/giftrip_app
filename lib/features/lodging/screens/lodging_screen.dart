c1fde12a4575415404bf38045714c0e5d3f2a474
amisu1203 <doh_0112@naver.com>
2025-05-11 21:41:27 +0900

    feat: 숙박 페이지 생성

---
+++ lib/features/lodging/screens/lodging_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:myong/core/widgets/banner/event_banner.dart';
+import 'package:myong/features/home/widgets/home_app_bar.dart';
+
+class LodgingScreen extends StatefulWidget {
+  const LodgingScreen({super.key});
+
+  @override
+  _LodgingScreenState createState() => _LodgingScreenState();
+}
+
+class _LodgingScreenState extends State<LodgingScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const HomeAppBar(),
+      body: Column(
+        children: [
+          const EventBannerWidget(),
+          Expanded(
+            child: ListView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+              children: [
+                Text('이벤트 페이지'),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
