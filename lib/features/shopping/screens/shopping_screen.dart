dae898d5c175c9d40edef0a7a46cddbb6db23d5d
amisu1203 <doh_0112@naver.com>
2025-05-11 21:41:31 +0900

    feat: 쇼핑 페이지 생성

---
+++ lib/features/shopping/screens/shopping_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:myong/core/widgets/banner/event_banner.dart';
+import 'package:myong/features/home/widgets/home_app_bar.dart';
+
+class ShoppingScreen extends StatefulWidget {
+  const ShoppingScreen({super.key});
+
+  @override
+  _ShoppingScreenState createState() => _ShoppingScreenState();
+}
+
+class _ShoppingScreenState extends State<ShoppingScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const HomeAppBar(),
+      body: Column(
+        children: [
+          const EventBannerWidget(),
+          Expanded(
+            child: ListView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+              children: [
+                Text('이벤트 페이지'),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
