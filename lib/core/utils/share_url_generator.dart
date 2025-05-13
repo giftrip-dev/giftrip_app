0c78f6540802eee4836a4c02d7d70b8b2cb8b172
amisu1203 <doh_0112@naver.com>
2025-05-13 17:57:43 +0900

    feat: 공유 url 생성 유틸리티

---
+++ lib/core/utils/share_url_generator.dart
@@ -0,0 +1,52 @@
+import 'package:myong/core/constants/item_type.dart';
+
+/// 공유 URL 생성 유틸리티
+class ShareUrlGenerator {
+  // todo: 실제 도메인으로 변경 필요
+  static const String _webDomain = 'myong.co.kr';
+
+  // 앱스토어 URL
+  static const String _androidAppUrl =
+      'https://play.google.com/store/apps/details?id=com.myong.app';
+  static const String _iosAppUrl =
+      'https://apps.apple.com/app/myong/id123456789';
+
+  /// 웹 URL 생성
+  static String generateWebUrl(String itemId, ProductItemType type) {
+    final path = switch (type) {
+      ProductItemType.product => 'products',
+      ProductItemType.accommodation => 'accommodations',
+      ProductItemType.experience => 'experiences',
+      ProductItemType.experienceGroup => 'experience-groups',
+    };
+
+    return 'https://$_webDomain/$path/$itemId';
+  }
+
+  /// 딥링크 URL 생성
+  static String generateDeepLink(String itemId, ProductItemType type) {
+    final path = switch (type) {
+      ProductItemType.product => 'products',
+      ProductItemType.accommodation => 'accommodations',
+      ProductItemType.experience => 'experiences',
+      ProductItemType.experienceGroup => 'experience-groups',
+    };
+
+    return 'myong://$path/$itemId';
+  }
+
+  /// 앱스토어 URL 생성 (Android/iOS 구분)
+  static String getAppStoreUrl(bool isIOS) {
+    return isIOS ? _iosAppUrl : _androidAppUrl;
+  }
+
+  /// 공유 메시지 생성
+  static String generateShareMessage({
+    required String title,
+    required String itemId,
+    required ProductItemType type,
+  }) {
+    final url = generateWebUrl(itemId, type);
+    return '[$title]\n\n자세히 보기 👉 $url';
+  }
+}
